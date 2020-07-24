45b5bd73d5db757092ea3c8929caa1e7083203d7
NStefaniuk <stefan2907@gmail.com>
2020-07-24 15:17:34 +0200

    Updates

---
+++ precache-manifest.f9d41c1de5049c5908ecf362aca4449e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "700e82577f9d5b35c869d1e8ff80e319",
+    "url": "/PIFinder/index.html"
+  },
+  {
+    "revision": "40caa53c7e82f54276d0",
+    "url": "/PIFinder/static/css/main.b76cb88a.chunk.css"
+  },
+  {
+    "revision": "afa3d245b82bf4fc5e51",
+    "url": "/PIFinder/static/js/2.d77acd97.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/PIFinder/static/js/2.d77acd97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40caa53c7e82f54276d0",
+    "url": "/PIFinder/static/js/main.65a4617f.chunk.js"
+  },
+  {
+    "revision": "e0142a330759abe795f3",
+    "url": "/PIFinder/static/js/runtime-main.6da5539c.js"
+  }
+]);
